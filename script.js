61fe3ffee867695789410ff9826dc322e77f3426
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-28 11:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let n=prompt(); 
+n=Number(n);
+let s=""
+for(let i=1;i<=n;i++){
+ // s=""
+	if(i%3 && i%5==0){
+		s=s+"FizzBuzz"+"\n";
+	}else
+	if(i%3==0){
+		s=s+"Fizz"+"\n";
+	}else
+	if(i%5==0){
+		s=s+"Buzz"+"\n";
+	}
+	
+}
+alert(s);
